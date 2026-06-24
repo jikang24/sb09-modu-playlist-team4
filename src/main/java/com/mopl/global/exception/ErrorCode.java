3de920866a97b8b28c0e936de2f016a18a6a39d9
jieun <clover6559@gmail.com>
2026-06-24 17:45:54 +0900

    [KAN-17] feat: 글로벌 공통 예외 처리(Exception Handling) 시스템 구현

---
+++ src/main/java/com/mopl/global/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.mopl.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    //사용자 관련
+    USER_NOT_FOUND("존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND),
+    DUPLICATE_EMAIL("이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
+    DUPLICATE_NAME("이미 사용 중인 이름입니다.", HttpStatus.CONFLICT),
+    INVALID_PASSWORD("현재 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+
+    private final String message;
+    private final HttpStatus status;
+}
