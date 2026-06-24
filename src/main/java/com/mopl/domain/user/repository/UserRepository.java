5356aff72242472a7b85d6cb54c24b7199e9d4b2
jieun <clover6559@gmail.com>
2026-06-24 17:29:19 +0900

    feat: 사용자(User) 비즈니스 로직 및 서비스 레이어 구현

---
+++ src/main/java/com/mopl/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mopl.domain.user.repository;
+
+import com.mopl.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(String email);
+
+    boolean existsByName(String name);
+
+}
