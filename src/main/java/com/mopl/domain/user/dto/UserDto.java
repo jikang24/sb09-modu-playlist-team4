38bf182806c45c1f55e3b3058e6d86c6fc06ad85
jieun <clover6559@gmail.com>
2026-06-24 10:24:02 +0900

    refactor: UserDto 생성일 타입을 Instant로 수정

--- src/main/java/com/mopl/domain/user/dto/UserDto.java
+++ src/main/java/com/mopl/domain/user/dto/UserDto.java
@@ -2,12 +2,12 @@ package com.mopl.domain.user.dto;
 
 import com.mopl.domain.user.domain.Role;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 
 public record UserDto(
         UUID id,
-        LocalDateTime createdAt,
+        Instant createdAt,
         String email,
         String name,
         String profileImageUrl,
