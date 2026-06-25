1ee8fbbc32d544101a1c307f7205e9a03406b203
jieun <clover6559@gmail.com>
2026-06-25 16:27:37 +0900

    chore: JWT 의존성 추가 및 프로젝트 환경 설정

--- src/main/java/com/mopl/MoplApplication.java
+++ src/main/java/com/mopl/MoplApplication.java
@@ -1,11 +1,14 @@
 package com.mopl;
 
+import com.mopl.global.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync  // @Async 이벤트 리스너 활성화
+@EnableConfigurationProperties(JwtProperties.class)  // JwtProperties 빈 등록
 public class MoplApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoplApplication.class, args);
