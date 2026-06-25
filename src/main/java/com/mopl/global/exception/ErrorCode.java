0f46ddd6d95f117aea9b17b0d77c9660a42f3f92
apple <kjw010900@gmail.com>
2026-06-25 17:04:49 +0900

    [KAN-18] feat: - ErrorCode: 콘텐츠, 리뷰, 플레이리스트 추가 - GlobalExceptionHandler: 400, 500에러 핸들러 추가

--- src/main/java/com/mopl/global/exception/ErrorCode.java
+++ src/main/java/com/mopl/global/exception/ErrorCode.java
@@ -10,7 +10,20 @@ public enum ErrorCode {
     USER_NOT_FOUND("존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND),
     DUPLICATE_EMAIL("이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
     DUPLICATE_NAME("이미 사용 중인 이름입니다.", HttpStatus.CONFLICT),
-    INVALID_PASSWORD("현재 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+    INVALID_PASSWORD("현재 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+
+    //Content
+    CONTENT_NOT_FOUND("콘텐츠를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    CONTENT_ALREADY_EXISTS("이미 존재하는 콘텐츠입니다.", HttpStatus.CONFLICT),
+
+    // Review 모듈
+    REVIEW_NOT_FOUND("리뷰를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    REVIEW_ALREADY_EXISTS("이미 리뷰를 작성했습니다.", HttpStatus.CONFLICT),
+    REVIEW_NOT_OWNER("본인의 리뷰만 수정/삭제할 수 있습니다.", HttpStatus.FORBIDDEN),
+
+    // Playlist 모듈
+    PLAYLIST_NOT_FOUND("플레이리스트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    PLAYLIST_NOT_OWNER("본인의 플레이리스트만 수정/삭제할 수 있습니다.", HttpStatus.FORBIDDEN);
 
     private final String message;
     private final HttpStatus status;
