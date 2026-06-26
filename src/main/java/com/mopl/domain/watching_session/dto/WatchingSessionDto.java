22432bb84358ac74cbaba14a1af13f2caad87661
apple <kjw010900@gmail.com>
2026-06-26 13:07:32 +0900

    [KAN-27] feat: watching_session 패키지 구조 정

---
+++ src/main/java/com/mopl/domain/watching_session/dto/WatchingSessionDto.java
@@ -0,0 +1,5 @@
+package com.mopl.domain.watching_session.dto;
+
+public class WatchingSessionDto {
+
+}
---
+++ src/main/java/com/mopl/domain/watching_session/service/WatchingSessionService.java
@@ -0,0 +1,5 @@
+package com.mopl.domain.watching_session.service;
+
+public class WatchingSessionService {
+
+}
