05e92acdc03b1e2a9140dd3ff4ddc9adc204433b
jieun <clover6559@gmail.com>
2026-06-29 09:18:08 +0900

    feat: mail 서비스 의존성 및 기본설정 구현

--- src/main/java/com/mopl/domain/auth/service/AuthService.java
+++ src/main/java/com/mopl/domain/auth/service/AuthService.java
@@ -1,4 +1,17 @@
 package com.mopl.domain.auth.service;
 
+import com.mopl.domain.auth.dto.JwtDto;
+import com.mopl.domain.auth.dto.ResetPasswordRequest;
+import com.mopl.domain.auth.dto.SignInRequest;
+
 public interface AuthService {
+    JwtDto signIn(SignInRequest request);
+
+    void signOut();
+
+    void resetPassword(ResetPasswordRequest request);
+
+    JwtDto refresh(String refreshToken);
+
+    void csrf();
 }
