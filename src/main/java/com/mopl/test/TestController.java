7f6ff54b9753c84a8e290e351e13190634d58d60
nano-mm <nano123@gachon.ac.kr>
2026-06-29 14:48:32 +0900

    fix: opensearch 인증 추가

--- src/main/java/com/mopl/test/TestController.java
+++ src/main/java/com/mopl/test/TestController.java
@@ -1,8 +1,14 @@
 package com.mopl.test;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +46,19 @@ public class TestController {
   @GetMapping("/test/opensearch")
   public String opensearch() {
     RestTemplate restTemplate = new RestTemplate();
-    return restTemplate.getForObject(openSearchUri, String.class);
+
+    // 마스터 계정 정보 적용
+    String auth = "mopl-master:Mopl1234!";
+    String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Authorization", "Basic " + encodedAuth);
+
+    HttpEntity<String> entity = new HttpEntity<>(headers);
+
+    // openSearchUri는 환경변수로 받아오신 엔드포인트가 들어갑니다.
+    ResponseEntity<String> response = restTemplate.exchange(openSearchUri, HttpMethod.GET, entity, String.class);
+
+    return response.getBody();
   }
 }
