6c24c615832e66678a627e617a8fbb8c6eba4d43
apple <kjw010900@gmail.com>
2026-06-30 16:11:14 +0900

    [KAN-37] feat: 외부 API 빈 등록을 위해 @ConfigurationPropertiesScan 어노테이션 추

--- src/main/java/com/mopl/MoplApplication.java
+++ src/main/java/com/mopl/MoplApplication.java
@@ -2,10 +2,12 @@ package com.mopl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync  // @Async 이벤트 리스너 활성화
+@ConfigurationPropertiesScan //외부 API 빈 등록 어노테이션
 public class MoplApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoplApplication.class, args);
