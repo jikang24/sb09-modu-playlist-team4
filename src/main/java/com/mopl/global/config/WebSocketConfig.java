0065c35bcd285cb0015382a3fba9d903c3ed8f67
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-07-02 22:32:24 +0900

    KAN-62 feat:directmesageSendRequest구현

--- src/main/java/com/mopl/global/config/WebSocketConfig.java
+++ src/main/java/com/mopl/global/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.mopl.global.config;
 
 
+import com.mopl.global.websocket.StompAuthInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,7 +12,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+  private final StompAuthInterceptor  stompAuthInterceptor;
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -21,6 +26,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void configureMessageBroker(MessageBrokerRegistry config) {
     config.enableSimpleBroker("/sub");
     config.setApplicationDestinationPrefixes("/pub");
+  }
+
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration){
+    registration.interceptors(stompAuthInterceptor);
   }
 
 
