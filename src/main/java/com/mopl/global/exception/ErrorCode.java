6819674fef535451e76ee9886b222dd6dc55fca0
jieun <clover6559@gmail.com>
2026-07-03 10:07:50 +0900

    feat: ErrorCode에 INVALID_FILE_EXTENSION, INVALID_IMAGE_FILE, S3_UPLOAD_FAILED 추가

--- src/main/java/com/mopl/global/exception/ErrorCode.java
+++ src/main/java/com/mopl/global/exception/ErrorCode.java
@@ -41,6 +41,8 @@ public enum ErrorCode {
     //s3
     S3_UPLOAD_FAILED("파일 업로드에 실패했습니다", HttpStatus.INTERNAL_SERVER_ERROR),
     S3_DELETE_FAILED("파일 삭제에 실패했습니다", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_FILE_EXTENSION("허용되지 않는 파일 확장자입니다", HttpStatus.BAD_REQUEST),
+    INVALID_IMAGE_FILE("올바른 이미지 파일이 아닙니다", HttpStatus.BAD_REQUEST),
 
     // Notification 모듈
     NOTIFICATION_NOT_FOUND("알림을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
