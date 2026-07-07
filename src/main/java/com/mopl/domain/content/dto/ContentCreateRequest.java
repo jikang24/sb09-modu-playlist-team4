56be9baf92ad23d8635d4a076e03f16072add55b
apple <kjw010900@gmail.com>
2026-07-07 15:34:06 +0900

    [KAN-81] refactor: Content 컨트롤러 및 dto 구조 전면 개편 - 외부 API id같이 swagger 구조와 다른 부분 수정, 컨트롤러 이중 매핑 제거 및 apiResponse 추가

--- src/main/java/com/mopl/domain/content/dto/ContentCreateRequest.java
+++ src/main/java/com/mopl/domain/content/dto/ContentCreateRequest.java
@@ -15,9 +15,6 @@ public record ContentCreateRequest(
     @NotNull(message = "콘텐츠 타입은 필수입니다.")
     ContentType type,
 
-    @NotBlank(message = "외부 ID는 필수입니다.")
-    String externalId,
-
     @NotBlank(message = "제목은 필수입니다.")
     @Size(max = 200, message = "제목은 200자 이하여야 합니다.")
     String title,
