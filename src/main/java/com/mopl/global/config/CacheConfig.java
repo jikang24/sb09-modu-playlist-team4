6dba1749a529136792595135994e309fe71e4683
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-07-13 13:25:22 +0900

    KAN-105 feat:CacheConfig구현

---
+++ src/main/java/com/mopl/global/config/CacheConfig.java
@@ -0,0 +1,40 @@
+package com.mopl.global.config;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+  @Bean
+  public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofMinutes(30))
+        .serializeKeysWith(RedisSerializationContext.SerializationPair
+            .fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(RedisSerializationContext.SerializationPair
+            .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+        .disableCachingNullValues();
+
+    Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+    cacheConfigs.put("userSummary", defaultConfig.entryTtl(Duration.ofHours(1)));
+    cacheConfigs.put("content", defaultConfig.entryTtl(Duration.ofMinutes(30)));
+    cacheConfigs.put("playlist", defaultConfig.entryTtl(Duration.ofMinutes(10)));
+
+    return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(defaultConfig)
+        .withInitialCacheConfigurations(cacheConfigs)
+        .build();
+  }
+}
