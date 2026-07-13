139fe05c36b19c53d98643cffbc1b250939daf24
jieun <clover6559@gmail.com>
2026-07-13 13:18:42 +0900

    fix : 에러코드 수정으로 인한 변경

--- src/main/java/com/mopl/global/exception/ErrorCode.java
+++ src/main/java/com/mopl/global/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
     DUPLICATE_NAME("이미 사용 중인 이름입니다.", HttpStatus.CONFLICT),
     INVALID_PASSWORD("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
     FORBIDDEN("접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
-    USER_LOCKED("계정이 잠금 처리되었습니다.", HttpStatus.FORBIDDEN),
+    ACCOUNT_LOCKED("계정이 잠금 처리되었습니다.", HttpStatus.FORBIDDEN),
     INVALID_CREDENTIALS("이메일 또는 비밀번호가 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
 
     //Content
@@ -84,7 +84,10 @@ public enum ErrorCode {
     TMDB_SERVER_ERROR("TMDB API 서버 오류입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
     // Batch
-    BATCH_EXECUTION_FAILED("배치 작업 실행에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    BATCH_EXECUTION_FAILED("배치 작업 실행에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+
+    //oauth
+    EMAIL_ALREADY_EXISTS("이미 존재하는 이메일입니다.", HttpStatus.CONFLICT);
 
     private final String message;
     private final HttpStatus status;
