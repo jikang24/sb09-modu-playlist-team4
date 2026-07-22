fd46c87db202270135f1e68e721bc00785b092ee
seunghyeonjeon <seunghyeonjeon57@gmail.com>
2026-07-22 09:22:31 +0900

    KAN-146 fix: replicas 개수 변경

--- src/main/java/com/mopl/infra/kafka/KafkaTopicConfig.java
+++ src/main/java/com/mopl/infra/kafka/KafkaTopicConfig.java
@@ -13,7 +13,7 @@ public class KafkaTopicConfig {
   public NewTopic notificationRequestedTopic() {
     return TopicBuilder.name(NotificationTopics.NOTIFICATION_REQUESTED)
         .partitions(3)
-        .replicas(1)
+        .replicas(3)
         .build();
   }
 }
